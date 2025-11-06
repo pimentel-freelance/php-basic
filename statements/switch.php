f8f05240025e7b3b408e9dda7618938e8cbe3188
pimentel-freelance <pimentel.freelance@gmail.com>
2025-11-06 11:09:57 -0400

    158. You Didn't Know the Switch Statement (Clothes Size Demo)

---
+++ statements/switch.php
@@ -0,0 +1,25 @@
+<?php 
+
+$size = "M";
+
+switch ($size) {
+    case "S":
+    case "M":
+        echo "Small or Medium size\n";
+        break;
+    case "L":
+    case "XL":
+        echo "Large or Extra Large size\n"; 
+        break;
+    default:
+        echo "Unknown size\n";        
+}
+
+$badAttempts = 2;
+
+switch ($badAttempts) {
+    case 3:
+        echo "You are blocked!\n";
+    default:
+        echo "Bad attempt detected!\n";    
+}
