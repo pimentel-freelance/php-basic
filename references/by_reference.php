83ee536654e497339c50ce268b28278bcf1af58d
pimentel-freelance <pimentel.freelance@gmail.com>
2025-11-07 11:19:10 -0400

    167. Passing by Value and Passing by Reference

---
+++ references/by_reference.php
@@ -0,0 +1,13 @@
+<?php 
+
+// No recomended, it use more memory.
+function doubleValue(int &$number): int {
+    $number *= 2;
+    return $number;
+}
+
+$original = 5;
+
+doubleValue($original);
+
+var_dump($original);
