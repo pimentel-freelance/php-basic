561025a8fa18daf3d6ed627c9b19d8155ab20cca
pimentel-freelance <pimentel.freelance@gmail.com>
2025-11-12 12:16:08 -0400

    186. Classes in PHP

---
+++ classes/classes1.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Person {
+    public function __construct(
+        public string $name,
+        public int $age
+        )
+        {}
+
+    public function introduce(): string {
+        return "Hi, I'm {$this->name} and I'm {$this->age} years old.\n";
+    }
+}
+
+$person = new Person("Alice", 40 );
+echo $person->introduce();
+
+$person = new Person("Anthony", 30 );
+echo $person->introduce();
